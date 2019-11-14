f4f6aa227d6d3519f183bba3542d19be538b6f0f
atsivsara <sivsaraat@gmail.com>
2019-11-14 19:10:51 +0530

    file download

---
+++ run_analysis.R
@@ -0,0 +1,6 @@
+# run analysis script
+# downloading the file and unziping the file
+if(!file.exists("./data")){dir.create("./data")}
+fileUrl <- "https://d396qusza40orc.cloudfront.net/getdata%2Fprojectfiles%2FUCI%20HAR%20Dataset.zip"
+download.file(fileUrl,destfile="./data/Dataset.zip")
+unzip(zipfile="./data/Dataset.zip",exdir="./data")
